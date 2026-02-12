4a871dd7c2485348a9e9293c876275d0dd2487a4
jakepreciado <jacobpreciado@gmail.com>
2026-02-11 23:04:08 -0700

    Form validation, dashboard connected to DB, conditional categories

--- FinanceTrackerApp/Models/Transaction.cs
+++ FinanceTrackerApp/Models/Transaction.cs
@@ -8,10 +8,19 @@ public class Transaction
     [Key]
     public Guid TransactionId { get; set; } = Guid.NewGuid();
     public string UserId { get; set; } = "demo-user"; // temporary for UI stage
+
+    [Required(ErrorMessage = "Amount is required!")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than 0!")]
     public decimal Amount { get; set; }
+
+    [Required(ErrorMessage = "Category is required!")]
     public string Category { get; set; } = "General";
     public DateTime Date { get; set; } = DateTime.Today;
+
+    [Required(ErrorMessage = "Description is required!")]
     public string Description { get; set; } = "";
-    // "income" or "expense"
+
+    [Required(ErrorMessage = "Type is required!")]
+    [RegularExpression("income|expense", ErrorMessage = "Type must be income or expense.")]
     public string Type { get; set; } = "expense";
 }
--- FinanceTrackerApp/obj/Debug/net10.0/FinanceTrackerApp.AssemblyInfo.cs
+++ FinanceTrackerApp/obj/Debug/net10.0/FinanceTrackerApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceTrackerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d1e4dd99729e890dc36325ccce5c2e6fc30fdc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c664520b718c82abd17207c6ad7a5daf5173e11")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceTrackerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceTrackerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
