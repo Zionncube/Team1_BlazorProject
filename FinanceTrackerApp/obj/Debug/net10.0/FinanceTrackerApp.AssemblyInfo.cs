30fc67158de38a0cc0d021dabacd45f2deb98b22
jakepreciado <jacobpreciado@gmail.com>
2026-02-12 07:26:29 -0700

    PR .gitignore cleanup

--- FinanceTrackerApp/obj/Debug/net10.0/FinanceTrackerApp.AssemblyInfo.cs
+++ FinanceTrackerApp/obj/Debug/net10.0/FinanceTrackerApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceTrackerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+247cf6ec5d3db7045954b98963a1529f92825ada")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f05e6a9800005682fe437e35480f4ae5d07e285e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceTrackerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceTrackerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
